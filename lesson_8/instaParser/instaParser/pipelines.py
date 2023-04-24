22628e905d73ee2939f01399fa555d57787a639d
Vladislav Goryakin <Happy.Seien@yandex.ru>
2023-04-24 21:14:15 +0300

    l-8
    l-8

---
+++ lesson_8/instaParser/instaParser/pipelines.py
@@ -0,0 +1,23 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import pymongo
+
+
+class InstaparserPipeline:
+    def __init__(self):
+        client = pymongo.MongoClient('localhost', 27017)
+        self.mongobase = client.instagram
+
+    def process_item(self, item, spider):
+        print()
+        collection = self.mongobase[item['user_parser_name']]
+        collection.create_index([('user_id', pymongo.TEXT)], name='unique description', unique=True)
+        del item['user_parser_name']
+        collection.insert_one(item)
+        return item
